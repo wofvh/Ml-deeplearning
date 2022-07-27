0d6aa0a5cda7a29f94c4e7937fdf36a1c53c6572
wofvh <wofvh@naver.com>
2022-07-27 12:44:30 +0900

    07/27프로젝트

--- keras_projact010.py/keras_image.py
+++ keras_projact010.py/keras_image.py
@@ -5,6 +5,6 @@ from urllib.request import urlopen
 
 response = urlopen('https://en.wikipedia.org/wiki/Main_Page')
 
-    soup = BeautifulSoup(response, 'html.parser')
-    for anchor in soup.find_all('a'):
-        print(anchor.get('href', '/'))
+soup = BeautifulSoup(response, 'html.parser')
+for anchor in soup.find_all('a'):
+    print(anchor.get('href', '/'))
--- study/keras/google.py
+++ study/keras/google.py
@@ -2,6 +2,6 @@ from google_images_download import google_images_download   #importing the libra
 
 response = google_images_download.googleimagesdownload()   #class instantiation
 
-arguments = {"keywords":"traiangle face shape,long face shape,정치인 관상","limit":50,"print_urls":True,"format":"jpg"}   #creating list of arguments
+arguments = {"keywords":"","limit":50,"print_urls":True,"format":"jpg"}   #creating list of arguments
 paths = response.download(arguments)   #passing the arguments to the function
 print(paths)   #printing absolute paths of the downloaded images 
