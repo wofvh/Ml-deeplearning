959062ece5fcc90210b7045b711a6489fcbbb0e4
wofvh <wofvh@naver.com>
2022-07-28 09:48:57 +0900

    07/28 프로젝트

--- keras/keras777_image.py
+++ keras/keras777_image.py
@@ -1,18 +1,51 @@
-from ast import keyword
-from urllib import response
-# pip install git+https://github.com/Joeclinton1/google-images-download.git
-from google_images_download import google_images_download
-from sympy import limit
+import cv2
+import numpy as np
+import os
 
-def googleImageCrawling(keyword, limit):
-    response = response = google_images_download.googleimagesdownload()  
 
-arguments = {"keywords":keyword,"limit": limit,
-             "print_urls": True,"chromedriver": "./chromedriver", "fromat":"jpg"}   
-paths = response.download(arguments)  
-print(paths)  
+path_dir = "D:\downloads\\asian_round_face_women"
+file_list = os.listdir(path_dir)
 
-keyword = input("키워드로 입력해주세요(복수 선정 시 ',' 붙여서 입력해주세요!): ")
-limit = input("이미지 개수를 입력해주세요 : " )
+#print(file_list)
 
-googleImageCrawling(keyword,"액소 백현","강다니엘", int(limit))
+file_name_list = []
+
+for i in range(len(file_list)):
+    file_name_list.append(file_list[i].replace(".jpg",""))
+
+print(file_name_list)
+
+#1장으로 테스트해본거. 잘됨
+
+# image = cv2.imread('img_14.jpg')
+# face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+# gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+# for (x,y,w,h) in faces:
+#     cv2.rectangle(image, (x,y), (x+w, y+h), (255,0,0), 2)
+#     cropped = image[y: y+h, x: x+w]
+#     resize = cv2.resize(cropped, (250,250))
+#     cv2.imshow("crop&resize", resize)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
+    
+
+
+def Cutting_face_save(image, name):
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        # cv2.rectangle(image, (x,y), (x+w, y+h), (255,0,0), 2)
+        cropped = image[y: y+h, x: x+w]
+        resize = cv2.resize(cropped, (250,250))
+        # cv2.imshow("crop&resize", resize)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+
+        # 이미지 저장하기
+        cv2.imwrite(f"D:\downloads\\asian_round_face_women/{name}.jpg", resize)
+        
+for name in file_name_list:
+    img = cv2.imread("D:\downloads\\asian_round_face_women/"+name+".jpg")
+    Cutting_face_save(img, name)
