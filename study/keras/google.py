5a6df50661b2f0fe0354f87115c9b7430ba67362
wofvh <wofvh@naver.com>
2022-07-28 21:01:28 +0900

    07/28

--- study/keras/google.py
+++ study/keras/google.py
@@ -2,7 +2,7 @@ from google_images_download import google_images_download   #importing the libra
 
 response = google_images_download.googleimagesdownload()   #class instantiation
 
-arguments = {"keywords":"oval face a female,oval face a mens asian,oval face a girl asian,\
-    ","limit":100,"print_urls":True,"format":"jpg"}   #creating list of arguments
+arguments = {"keywords":"문근영 얼굴,송중기 얼굴,\
+    안철수 얼굴,","limit":90,"print_urls":True}   #creating list of arguments
 paths = response.download(arguments)   #passing the arguments to the function
-print(paths)   #printing absolute paths of the downloaded images 
+print(paths)   #printing absolute paths of the downloaded images
