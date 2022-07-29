fb05b0f7333250a84a12727ea4388bf19ddfe62a
wofvh <wofvh@naver.com>
2022-07-29 19:59:00 +0900

    07/29 프로젝트

--- keras/keras1000_projact_test.py
+++ keras/keras1000_projact_test.py
@@ -3,6 +3,13 @@ import pandas as pd
 import numpy as np
 import cv2
 import math
+from tensorflow.python.keras.models import Sequential, Model
+from tensorflow.python.keras.layers import Activation, Dense, Conv2D, Flatten, MaxPooling2D, Input, Dropout
+from tensorflow.python.keras.utils.np_utils import to_categorical
+from keras.applications.vgg16 import VGG16 
+from keras.preprocessing import image 
+from keras.applications.vgg16 import preprocess_input 
+
 
 ## 데이터 제네레이터##
 ## 쓰는 이유 : 이미지를 한 덩어리 안에 집어넣으면 램이 터지기때문에 학습할때 쪼개서 들어갈 수 있도록 class형식으로 만들어서 넣어줌
@@ -64,7 +71,7 @@ class DataGenerator(keras.utils.Sequence):
 from sklearn.model_selection import StratifiedKFold
 import pandas as pd
 
-data = pd.read_csv('c:/hoon/cls_data.csv',index_col=0)
+data = pd.read_csv('./_data/project/cls_data.csv',index_col=0)
 skf = StratifiedKFold(n_splits=8, shuffle = True, random_state=42)
 
 data['fold'] = -1
@@ -73,7 +80,33 @@ for idx, (t, v) in enumerate(skf.split(data, data['label']), 1):
     data.loc[v, 'fold'] = idx
 
 
-train_gen = DataGenerator(batch_size=32,df = data, mode = 'train', img_size= img_size)
-valid_gen = DataGenerator(batch_size=32, df = data, mode = 'valid',img_size = img_size)
+train_gen = DataGenerator(batch_size=4, df = data, mode = 'train', image_size = 128)
+valid_gen = DataGenerator(batch_size=4, df = data, mode = 'valid', image_size = 128)
 
-            
+print(train_gen[0][0].shape)  #(4, 128, 128, 3)
+print(train_gen[0][1].shape)  #(4,)
+print(valid_gen[0][0].shape)  #(4, 128, 128, 3)
+print(valid_gen[0][1].shape)  #(4,)
+
+
+#모델구성 
+
+model = Sequential()
+model.add(Conv2D(35,(2,2),input_shape = (128,128,3), activation='relu'))
+model.add(Conv2D(64,(3,3),activation= 'relu'))
+model.add(Flatten())
+model.add(Dense(512,activation='relu'))
+model.add(Dense(256,activation='relu'))
+model.add(Dense(128,activation='relu'))
+model.add(Dense(64,activation='relu'))
+model.add(Dense(32,activation='relu'))
+model.add(Dense(30,activation='softmax'))
+model.summary()
+
+print(train_gen)
+
+
+#3. 컴파일, 훈련
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(train_gen , validation_data = valid_gen , epochs=30)
+
