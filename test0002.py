8b64124950af4da64eabdd1dcd9b272fb96eb091
wofvh <wofvh@naver.com>
2022-07-31 22:48:41 +0900

    07/31

--- test0002.py
+++ test0002.py
@@ -1,3 +1,16 @@
-import tensorflow as tf
+# class Car:
+#     #클래스의 생성자
+#     def __init__(self, name , color):
+#         self.name = name
+#         self.color = color
+        
+#         def show_info(self):
+#             print("이름", self.name, "/색상:", self.color)
 
-print("텐서플로우 버전 : ", tf.__version__)
+# car1 = Car("소나타", "빨간색")
+# car1.show_info()
+
+
+def hello():
+    print("sxe")
+hello()
