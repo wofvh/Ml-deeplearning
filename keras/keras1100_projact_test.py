3cbd1fc6c85d4494180ee043d78651ccc56980a4
wofvh <wofvh@naver.com>
2022-08-02 01:51:03 +0900

    08/02

--- keras/keras1100_projact_test.py
+++ keras/keras1100_projact_test.py
@@ -10,14 +10,21 @@ from tensorflow.python.keras.layers import Activation, Dense, Conv2D, Flatten, M
 from tensorflow.python.keras.utils.np_utils import to_categorical
 from sklearn.model_selection import StratifiedKFold
 
+
+
+# 데이터 제네레이터##
+## 쓰는 이유 : 이미지를 한 덩어리 안에 집어넣으면 램이 터지기때문에 학습할때 쪼개서 들어갈 수 있도록 class형식으로 만들어서 넣어줌
+
 # Define Generator
+
+
 class DataGenerator(keras.utils.Sequence):
     def __init__(self, batch_size, df, image_size, mode='train', shuffle=True): # 생성자 : 배치사이즈, 데이터프레임, 모드(학습,검증) , 셔플(섞어서들어가기)
         self.batch_size = batch_size 
         self.mode = mode
         self.image_size = image_size
         self.shuffle = shuffle
-        self.df = df
+        self.df = df                 # df = 데이터 파라미터 ()
  
         if self.mode == 'train': # mode가 train일때 검증용 데이터 (지금은 fold == 1 ) 빼고 학습을 진행
             self.df = self.df[self.df['fold'] != 1]
@@ -68,7 +75,7 @@ class DataGenerator(keras.utils.Sequence):
 #################################################################################
 ################################# PATH 보완 #####################################
 
-data = pd.read_csv('./_data/project/cls_data.csv',index_col=1)
+data = pd.read_csv('./_data/project/cls_data.csv',index_col=0)
 
 # data['image_path'] = data['image_path'].apply(cp)
 # Fold
@@ -79,12 +86,13 @@ data['fold'] = -1
 for idx, (t, v) in enumerate(skf.split(data, data['label']), 1):
     data.loc[v, 'fold'] = idx
     
-train_gen = DataGenerator(batch_size=4, df = data, mode = 'train', image_size = 94)
-valid_gen = DataGenerator(batch_size=4, df = data, mode = 'valid', image_size = 94)
+train_gen = DataGenerator(batch_size=4, df = data, mode = 'train', image_size = 100)
+valid_gen = DataGenerator(batch_size=4, df = data, mode = 'valid', image_size = 100)
 
 
 print(data.shape)
 
+'''
 model = Sequential()
 model.add(Conv2D(filters=64,kernel_size=(2, 2), padding='same', input_shape=(94,94,3), activation='relu'))
 model.add(MaxPooling2D())
@@ -101,6 +109,36 @@ model.summary()
 print(data)
 
 
-#3. 컴파일, 훈련\
+#3. 컴파일, 훈련
 model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.fit(train_gen , validation_data = valid_gen , epochs=10)
+'''
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense , Conv2D , Flatten,MaxPooling2D
+
+
+model = Sequential()
+model.add(Conv2D(filters=64,kernel_size=(2, 2), padding='same', input_shape=(100,100,3), activation='relu'))
+model.add(MaxPooling2D())
+model.add(Conv2D(512,(3,3), activation='relu'))
+model.add(MaxPooling2D())
+model.add(Conv2D(256,(3,3), activation='relu'))
+model.add(MaxPooling2D())
+model.add(Conv2D(128,(3,3), activation='relu'))
+model.add(Flatten())
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(30, activation='softmax'))
+model.summary()
+
+#3.컴파일 훈련
+model.compile(loss= 'sparse_categorical_crossentropy',optimizer='adam', metrics=['accuracy'])
+
+#model.fit(cy_train[0][0], xy_train[0][1])# 배치를 최대로 자으면 이것도 가능 
+hist = model.fit_generator(train_gen ,epochs=30,steps_per_epoch=32,
+                                            #전체데이터/batch = 160/5 = 32
+                    validation_data =valid_gen, )
+accuracy = hist.history['accuracy']
+val_accuracy = hist.history['val_accuracy']
+loss = hist.history['loss']
+val_loss = hist.history['val_loss']
