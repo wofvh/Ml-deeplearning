51c51ef46f16dd996e88ab744df59e950ce8904f
wofvh <wofvh@naver.com>
2022-08-03 09:49:12 +0900

    08/02

--- keras/keras1100_projact_test_test.py
+++ keras/keras1100_projact_test_test.py
@@ -1,4 +1,4 @@
-from datetime import date
+import tensorflow as tf
 from mimetypes import init
 from tensorflow import keras
 import pandas as pd
@@ -6,15 +6,10 @@ import numpy as np
 import cv2
 import math
 from tensorflow.python.keras.models import Sequential, Model
-from tensorflow.python.keras.layers import Activation, Dense, Conv2D, Flatten, MaxPooling2D, Input, Dropout,Conv1D
+from tensorflow.python.keras.layers import Activation, Dense, Conv2D, Flatten, MaxPooling2D, Input, Dropout
 from tensorflow.python.keras.utils.np_utils import to_categorical
-from keras.applications.vgg16 import VGG16 
-from keras.preprocessing import image 
-from keras.applications.vgg16 import preprocess_input 
 from sklearn.model_selection import StratifiedKFold
-import matplotlib.pyplot as plt
-from keras.preprocessing.image import ImageDataGenerator
-
+# Define Generator
 class DataGenerator(keras.utils.Sequence):
     def __init__(self, batch_size, df, image_size, mode='train', shuffle=True): # 생성자 : 배치사이즈, 데이터프레임, 모드(학습,검증) , 셔플(섞어서들어가기)
         self.batch_size = batch_size 
@@ -25,7 +20,7 @@ class DataGenerator(keras.utils.Sequence):
  
         if self.mode == 'train': # mode가 train일때 검증용 데이터 (지금은 fold == 1 ) 빼고 학습을 진행
             self.df = self.df[self.df['fold'] != 1]
-        elif self.mode =='valid': # mode 가 validation(검증) 일때는 얘만 따로 학습검증을함
+        elif self.mode =='val': # mode 가 validation(검증) 일때는 얘만 따로 학습검증을함
             self.df = self.df[self.df['fold'] == 1]
 
         self.on_epoch_end()
@@ -40,8 +35,8 @@ class DataGenerator(keras.utils.Sequence):
         fin = (idx+1) * self.batch_size
         data = self.df.iloc[strt:fin] # ex) idx가 0 이고 배치사이즈 64 일 경우  dataframe의 [0:64] 그다음은 [64:128]로 찢어서 나갈 수 있게 iterator(반복자) 개념 필요
         batch_x, batch_y = self.get_data(data) # 아래 정의된 get data에서 데이터 받아옴
-        
-        return np.array(batch_x), np.array(batch_y).reshape(-1,1) 
+
+        return np.array(batch_x), np.array(batch_y)
 
 
     def get_data(self, data):
@@ -51,7 +46,7 @@ class DataGenerator(keras.utils.Sequence):
         for _ , r in data.iterrows(): # data iterrows 로 한개씩 훑고 지나감(이미지를 불러오기 위함) 연습필요 당장 이해 힘듬
             file_path = r['image_path'] 
             
-            image = plt.imread(file_path) #이미지 불러오고
+            image = cv2.imread(file_path) #이미지 불러오고
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) #cvtColor 할거임
 
             image = cv2.resize(image , (self.image_size , self.image_size)) # model input size로 resize 해주기
@@ -73,25 +68,22 @@ class DataGenerator(keras.utils.Sequence):
 ################################# PATH 보완 #####################################
 #################################################################################
 
-
-data = pd.read_csv('./_data/project/cls_data.csv',index_col=0)
+data = pd.read_csv('c:/help_temp/cls_data.csv', index_col=0)
 # data['image_path'] = data['image_path'].apply(cp)
 # Fold
 skf = StratifiedKFold(n_splits=8, shuffle = True, random_state=42)
 data['fold'] = -1
 
+
 for idx, (t, v) in enumerate(skf.split(data, data['label']), 1):
     data.loc[v, 'fold'] = idx
     
-for batch in data:
-    x,y = batch
-    print(x)
-    print(y)
-    print(y.shape)
-    break
-'''
+train_gen = DataGenerator(batch_size=16, df = data, mode = 'train', image_size = 94)
+valid_gen = DataGenerator(batch_size=16, df = data, mode = 'valid', image_size = 94)
+    
+    
 model = Sequential()
-model.add(Conv2D(filters=64 ,kernel_size=(2, 2), padding='same', input_shape=(94,94,3), activation='relu'))
+model.add(Conv2D(filters=64,kernel_size=(3, 3), padding='same', input_shape=(94,94,3), activation='relu'))
 model.add(MaxPooling2D())
 model.add(Conv2D(512,(3,3), activation='relu'))
 model.add(MaxPooling2D())
@@ -102,14 +94,17 @@ model.add(Flatten())
 model.add(Dense(64, activation='relu'))
 model.add(Dense(32, activation='relu'))
 model.add(Dense(30, activation='softmax'))
-model.summary()
+# model.summary()
 
 
-#3. 컴파일, 훈련
+
+
+#3. 컴파일, 훈련\
 model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(train_gen , validation_data = valid_gen , epochs=10)
+model.fit(train_gen , validation_data = valid_gen , epochs=30)
 
 
+'''
 #4. 평가, 예측
 loss = model.evaluate([train_gen ]) 
 y_predict = model.predict(valid_gen)
