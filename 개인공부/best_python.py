cc3e9bf252a215248f43e5db36dadd13725ea30b
wofvh <wofvh@naver.com>
2022-08-11 19:56:36 +0900

    08/11

--- 개인공부/best_python.py
+++ 개인공부/best_python.py
@@ -21,3 +21,15 @@ print(y.shape)
 le = LabelEncoder()
 y = le.fit_transform(y)
 print(y.shape)
+
+datainfo= ["id,Age,TypeofContact,CityTier,DurationOfPitch,Occupation,Gender,NumberOfPersonVisiting,\
+    NumberOfFollowups,ProductPitched,PreferredPropertyStar,MaritalStatus,NumberOfTrips,Passport,\
+    PitchSatisfactionScore,OwnCar,NumberOfChildrenVisiting,Designation,MonthlyIncome"
+]
+
+
+tokorean = ['샘플 아이디', '나이','고객의 제품 인지 방법 ', '주거 중인 도시의 등급', '영업 사원이 고객에게 제공하는 프레젠테이션 기간',
+                '직업', '성별', '고객과 함께 여행을 계획 중인 총 인원', '영업 사원의 프레젠테이션 후 이루어진 후속 조치 수',
+                '영업 사원이 제시한 상품', '선호 호텔 숙박업소 등급', '결혼여부', '평균 연간 여행 횟수', '여권 보유 여부 (0: 없음, 1: 있음)',
+                '영업 사원의 프레젠테이션 만족도', '자동차 보유 여부', '함께 여행을 계획 중인 5세 미만의 어린이 수', '(직업의) 직급',
+                '월 급여',]
