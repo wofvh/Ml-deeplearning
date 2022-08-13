82bf761ac35e93e29101c0125ff31d2d443de608
wofvh <wofvh@naver.com>
2022-08-13 14:46:24 +0900

    08/13

--- selenium/개인공부/best_python.py
+++ selenium/개인공부/best_python.py
@@ -1,128 +1,41 @@
-# 문제 풀기 
-# from tensorflow.python.keras.models import Sequential,load_model
-from tensorflow.python.keras.layers import Dense,Dropout,Conv2D,Flatten,LSTM,Conv1D
-from sklearn.model_selection import train_test_split
-from tensorflow.python.keras.callbacks import EarlyStopping,ModelCheckpoint
-import matplotlib.pyplot as plt
-from sklearn.model_selection import RandomizedSearchCV
-from sklearn.metrics import r2_score
-import matplotlib
-from sklearn.experimental import enable_halving_search_cv
-from sklearn.model_selection import HalvingGridSearchCV
-from sklearn.model_selection import HalvingRandomSearchCV
-matplotlib.rcParams['font.family']='Malgun Gothic'
-matplotlib.rcParams['axes.unicode_minus']=False
-import pandas as pd
-from sklearn.svm import LinearSVC 
-from sklearn.svm import LinearSVR 
+# a_string = 'like this' #str = string 문자  
+# a_number = 3           #number 숫자
+# a_float = 3.12         #float 소수점 있는 숫자  
+# a_boolean  = False     #boolean True or False 진실 or 거짓 
+#                        #int = integer 정수를 의미함 소수점 없는 숫자     
+# a_none = None          #None 비어있다는걸 의미함 
+
+# print(type(a_none))  
+
+# days =  ("mon","tue","wed","thur","fri","sat" )
+ 
+# # print(days)
+# # days.append("sat")
+# # days.reverse()          #reverse 리스안에 문자를 역방향으로 바꿔줄때사용 
+# print(type(days))
+
+# nico = {                    #dictionary 한 딕셔너리 안에 정보를 넣어사전 처럼 만듬 
+#     "name" : "nico",
+#     "age": 29 ,
+#     "korean" : True,
+#     "fav_food": ["kimchi",
+#     "sashimi"]
+# }
+# print(nico)
+# nico["handsome"] =True
+# print(nico)
 
 
-#1. 데이터
-path = './_data/travel/'
-train_set = pd.read_csv(path + 'train.csv',
-                        index_col=0)
-print(train_set)
+# something = ("tntt", True,15,None,False,"laslaslal")
 
-print(train_set.shape) #(1459, 10)
+# print(something)
 
-test_set = pd.read_csv(path + 'test.csv', #예측에서 쓸거야!!
-                       index_col=0)
+# age = "18"
+# print(age)
+# print(type(age))
+# n_age = (int(age))
+# print(n_age)
+# print(type(n_age))
 
-
-submission = pd.read_csv(path + 'sample_submission.csv',#예측에서 쓸거야!!
-                       index_col=0)
-                       
-print(test_set)          #[2933 rows x 18 columns]
-print(test_set.shape)    #(2933, 18)
-
-print(train_set)          #[1955 rows x 19 columns]
-print(train_set.shape)    #(1955, 19)      
-
-
-print(train_set.isna().sum())
-#train_set[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean().sort_values(by='Survived', ascending=False)
-#train_set[["Sex", "Survived"]].groupby(['Sex'], as_index=False).mean().sort_values(by='Survived', ascending=False)
-#train_set[["SibSp", "Survived"]].groupby(['SibSp'], as_index=False).mean().sort_values(by='Survived', ascending=False)
-#train_set[["Parch", "Survived"]].groupby(['Parch'], as_index=False).mean().sort_values(by='Survived', ascending=False)
-
-def handle_na(data):
-    temp = data.copy()
-    for col, dtype in temp.dtypes.items():
-        if dtype == 'object':
-            #문자형 컬럼의 경우 "unknow"k 를 채워줌
-            value ='unknown'
-        elif dtype == int or dtype == float:
-            #수치형 컬럼의 경우0을 채워줌
-            value = 0
-        temp.loc[:,col] = temp[col].fillna(value)
-    return temp
-
-train_set = handle_na(train_set)
-
-print(train_set.isna().sum())
-
-e = LabelEncoder()
-y = le.fit_transform(y)
-print(y.shape)
-
-# pca = PCA(n_components=20)   #54 >10
-# x = pca.fit_transform(x)
-
-lda = LinearDiscriminantAnalysis(n_components=1)
-lda.fit(x,y)
-x = lda.transform(x)
-print(x.shape)
-print(y.shape)
-
-
-
-print(train_set.columns)
-print(train_set.info()) #null은 누락된 값이라고 하고 "결측치"라고도 한다.
-print(train_set.describe()) 
-
-###### 결측치 처리 1.제거##### dropna 사용
-print(train_set.isnull().sum()) #각 컬럼당 결측치의 합계
-train_set = train_set.fillna(train_set.median())
-print(train_set.isnull().sum())
-print(train_set.shape)
-test_set = test_set.fillna(test_set.median())
-
-x = train_set.drop(['count'],axis=1) #axis는 컬럼 
-print(x.columns)
-print(x.shape) #(1459, 9)
-
-y = train_set['count']
-
-from sklearn.model_selection import KFold,cross_val_score,GridSearchCV,StratifiedKFold
-from sklearn.ensemble import RandomForestRegressor #공부하자 
-
-x_train, x_test ,y_train, y_test = train_test_split(
-          x, y, train_size=0.8,shuffle=True,random_state=100)
-kfold = KFold(n_splits=5, shuffle=True, random_state=66)
-from sklearn.preprocessing import MinMaxScaler,StandardScaler
-# scaler = StandardScaler()
-# x_train = scaler.fit_transform(x_train)
-# x_test = scaler.transform(x_test)
-
-
-# parameters = [
-#     {'n_estimators':[100, 200],'max_depth':[6, 8],'min_samples_leaf':[3,5],
-#      'min_samples_split':[2, 3],'n_jobs':[-1, 2]},
-#     {'n_estimators':[300, 400],'max_depth':[6, 8],'min_samples_leaf':[7, 10],
-#      'min_samples_split':[4, 7],'n_jobs':[-1, 4]}
-#     ]     
-from sklearn.svm import LinearSVC, SVC
-from sklearn.ensemble import RandomForestClassifier,RandomForestRegressor
-from sklearn.pipeline import make_pipeline
-
-# model= SVC()
-model = make_pipeline(MinMaxScaler(),RandomForestRegressor())
-
-#훈련
-model.fit(x_train,y_train)
-
-#평가예측
-result = model.score(x_test,y_test)
-
-print("model.score:", result)
-'''
+def say_hello():
+    print("hello")
