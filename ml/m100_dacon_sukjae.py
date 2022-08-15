5a0b1ac672c7b038be92d8e1f7da89d05bb1f738
wofvh <wofvh@naver.com>
2022-08-15 23:53:35 +0900

    08/15

--- ml/m100_dacon_sukjae.py
+++ ml/m100_dacon_sukjae.py
@@ -1,61 +1,123 @@
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.rcParams['font.family']='Malgun Gothic'
+matplotlib.rcParams['axes.unicode_minus']=False
 import pandas as pd
-import random
-import os
-import numpy as np
-from sklearn.metrics import mean_squared_error
+import numpy as np 
+from sklearn.model_selection import KFold, StratifiedKFold, train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.feature_selection import SelectFromModel
+from sklearn.decomposition import PCA
 
-from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.multioutput import MultiOutputRegressor
-from xgboost import XGBRegressor
-from sklearn.experimental import enable_iterative_imputer
-from sklearn.impute import IterativeImputer, KNNImputer
+#1. 데이터
+path = './_data/prodacon/' # ".은 현재 폴더"
+train_set = pd.read_csv(path + 'train.csv',
+                        index_col=0)
+test_set = pd.read_csv(path + 'test.csv', #예측에서 쓸거야!!
+                       ).drop(columns=['ID'])
+# print(train_set.shape)  #(39607, 71)      
 
+train_x = train_set.filter(regex='X') # Input : X Featrue : 56
+train_y = train_set.filter(regex='Y') # Output : Y Feature : 14
+# print(test_set.isnull().sum())
+# print(train_y.isnull().sum())
+pca = PCA(n_components=17) # 차원 축소 (차원=컬럼,열,피처)
 
-def seed_everything(seed):
-    random.seed(seed)
-    os.environ['PYTHONHASHSEED'] = str(seed)
-    np.random.seed(seed)
-seed_everything(704) # Seed 고정
+x = pca.fit_transform(train_x)
+test_set = pca.transform(test_set) 
+# pca_EVR = pca.explained_variance_ratio_ # PCA로 압축 후에 새로 생성된 피쳐 임포턴스를 보여준다.
+# print(sum(pca_EVR)) #0.999998352533973
+# print(pca_EVR)
 
-filepath = './_data/prodacon/'
-
-train = pd.read_csv(filepath + 'train.csv',index_col=0)
-test = pd.read_csv(filepath + 'test.csv').drop(columns=['ID'])
-
-
-train_x = train.filter(regex='X') # Input : X Featrue
-train_y = train.filter(regex='Y') # Output : Y Feature
-
-cols = ["X_10","X_11"]
-train[cols] = train[cols].replace(0, np.nan)
-
-# train[cols].fillna(train[cols].mean(), inplace=True)
-
-# imp = KNNImputer()
-
-imp = IterativeImputer(estimator = LinearRegression(), 
-                       tol= 1e-10, 
-                       max_iter=30, 
-                       verbose=2, 
-                       imputation_order='roman')
-
-train = imp.fit_transform(train)
+# cumsum = np.cumsum(pca_EVR)
+# print(cumsum)
+# import matplotlib.pyplot as plt   
+# plt.plot(cumsum)
+# plt.grid()
+# plt.show()
 
 
-# model = MultiOutputRegressor(XGBRegressor(n_estimators=150, learning_rate=0.08, gamma = 1, subsample=0.75, colsample_bytree = 1, max_depth=7) )
-model = MultiOutputRegressor(LinearRegression())
-# model = RandomForestRegressor()
+x_train,x_test,y_train,y_test=train_test_split(train_x,train_y,shuffle=True,random_state=1234,train_size=0.9)
+# print(train_x.shape,train_y.shape)  #(39607, 56) (39607, 14)     
+# print(test_set.shape) # (39608, 56)
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, MaxAbsScaler, QuantileTransformer, PowerTransformer
+# scaler = StandardScaler() 
+# scaler = MinMaxScaler()
+# x_train = scaler.fit_transform(x_train)
+# x_test= scaler.transform(x_test)
+from xgboost import XGBClassifier,XGBRegressor
+from sklearn.metrics import r2_score
 
-model.fit(train_x, train_y)
-preds = model.predict(test)
-print(model.score(train_x, train_y))
+# 2. 모델
 
-submit = pd.read_csv(filepath +'sample_submission.csv')
-for idx, col in enumerate(submit.columns):
+n_splits = 5
+kfold = KFold(n_splits=n_splits,shuffle=True,random_state=123)
+# parameters = {'n_estimators':[100,200,300,400,500,1000], # 디폴트 100/ 1~inf 무한대 
+# eta[기본값=0.3, 별칭: learning_rate] learning_rate':[0.1,0.2,0.3,0.4,0.5,0.7,1]
+# max_depth': [1,2,3,4,5,6,7][기본값=6]
+# gamma[기본값=0, 별칭: min_split_loss] [0,0.1,0.3,0.5,0.7,0.8,0.9,1]
+# min_child_weight[기본값=1] 0~inf
+# subsample[기본값=1][0,0.1,0.3,0.5,0.7,1] 0~1
+# colsample_bytree [0,0.1,0.2,0.3,0.5,0.7,1]    [기본값=1] 0~1
+# colsample_bylevel': [0,0.1,0.2,0.3,0.5,0.7,1] [기본값=1] 0~1
+# 'colsample_bynode': [0,0.1,0.2,0.3,0.5,0.7,1] [기본값=1] 0~1
+# 'reg_alpha' : [0,0.1 ,0.01, 0.001, 1 ,2 ,10]  [기본값=0] 0~inf /L1 절댓값 가중치 규제 
+# 'reg_lambda' : [0,0.1 ,0.01, 0.001, 1 ,2 ,10]  [기본값=1] 0~inf /L2 절댓값 가중치 규제 
+####
+parameters = {'n_estimators':[100,200],
+              'learning_rate':[0.1,0.2,0.3],
+              'max_depth': [5,6,7],
+              'gamma' : [0,0.1,0.3],
+            #   'min_child_weight' : [1],
+            #   'subsample' : [1],
+            #   'colsample_bytree' : [0.5],
+            #   'colsample_bylevel': [1],
+            #   'colsample_bynode': [1],
+            #   'alpha' : [0],
+            #   'lambda' : [0]
+              } # 디폴트 6 
+
+#2. 모델 
+xgb = XGBRegressor(random_state=123,
+                    n_estimators=100,
+                    # tree_method='gpu_hist'
+                )
+
+model = GridSearchCV(xgb,parameters,cv=kfold,n_jobs=-1)
+import time
+start_time = time.time()
+model.fit(x_train,y_train)
+end_time = time.time() - start_time
+# model.score(x_test,y_test)
+results = model.score(x_test,y_test)
+
+print('최적의 매개변수 : ',model.best_params_)
+print('최상의 점수 : ',model.best_score_)
+print('걸린 시간 : ',end_time)
+print('model.socre : ',results)
+y_summit = model.predict(test_set)
+submission = pd.read_csv(path + 'sample_submission.csv',#예측에서 쓸거야!!
+                     )
+# print(submission)
+for idx, col in enumerate(submission.columns):
     if col=='ID':
         continue
-    submit[col] = preds[:,idx-1]
+    submission[col] = y_summit[:,idx-1]
+print('Done.')
+submission.to_csv('test23.csv',index=False)
 
-submit.to_csv(filepath + 'submission.csv', index=False)
+# 최적의 매개변수 :  {'learning_rate': 0.1, 'n_estimators': 100}
+# 최상의 점수 :  0.06523837244168644
+# 걸린 시간 :  60.23209834098816
+# model.socre :  0.06985114732540963 
 
+# 최적의 매개변수 :  {'learning_rate': 0.1, 'n_estimators': 100}
+# 최상의 점수 :  0.06577843806588209
+# 걸린 시간 :  309.3102035522461
+# model.socre :  0.0678021185
+
+# 최적의 매개변수 :  {'learning_rate': 0.1, 'n_estimators': 100}
+# 최상의 점수 :  0.06970460808498297
+# 걸린 시간 :  329.5538504123688
+# model.socre :  0.07454421428426697
--- ml/m101_dacon_sukjae.py
+++ ml/m101_dacon_sukjae.py
@@ -1,143 +1,312 @@
-import numpy as np
 import pandas as pd
-from sklearn.datasets import load_iris
-from sklearn.model_selection import KFold,cross_val_score,GridSearchCV,StratifiedKFold, RandomizedSearchCV
-from sklearn.experimental import enable_halving_search_cv
-from sklearn.model_selection import HalvingGridSearchCV, HalvingRandomSearchCV
-from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler, MaxAbsScaler
-from sklearn.metrics import accuracy_score, r2_score
-from sklearn.model_selection import train_test_split
-from sklearn.experimental import enable_iterative_imputer # 이터러블 입력시 사용하는 모듈 추가
-from sklearn.impute import SimpleImputer, KNNImputer, IterativeImputer 
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-from lightgbm import LGBMClassifier, LGBMRegressor
-import warnings
-warnings.filterwarnings('ignore')
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
-from sklearn.feature_selection import SelectFromModel
-
-
-
-
-#1. 데이터
-path = './_data/travel/'
-train = pd.read_csv(path + 'train.csv', # + 명령어는 문자를 앞문자와 더해줌
-                        index_col=0) # index_col=n n번째 컬럼을 인덱스로 인식
-
-test = pd.read_csv(path + 'test.csv', # 예측에서 쓸거임                
-                       index_col=0)
-# 결측치를 처리하는 함수를 작성.
-# drop_col = ['NumberOfChildrenVisiting','TypeofContact','OwnCar','NumberOfPersonVisiting'] # 컬럼 삭제하기 위한 리스트 생성
-# train = train.drop(drop_col, axis=1) # axis=1 : 세로, axis=0 : 가로
-# test = test.drop(drop_col, axis=1) # 결측치 처리하기 위한 함수 실행
-
-def handle_na(data):
-    temp = data.copy()
-    for col, dtype in temp.dtypes.items():
-        if dtype == 'object':
-            # 문자형 칼럼의 경우 'Unknown'
-            value = 'Unknown'
-        elif dtype == int or dtype == float:
-            # 수치형 칼럼의 경우 0
-            value = 0
-        temp.loc[:,col] = temp[col].fillna(value)
-    return temp
-
-train_nona = handle_na(train)
-
-# 결측치 처리가 잘 되었는지 확인해 줍니다.
-train_nona.isna().sum()
-
-print(train_nona.isna().sum())
-object_columns = train_nona.columns[train_nona.dtypes == 'object']
-print('object 칼럼 : ', list(object_columns))
-
-# 해당 칼럼만 보아서 봅시다
-train_nona[object_columns]
-
-# LabelEncoder를 준비해줍니다.
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from xgboost import XGBClassifier, XGBRFClassifier
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.preprocessing import LabelEncoder
-
-encoder = LabelEncoder()
-
-# LabelEcoder는 학습하는 과정을 필요로 합니다.
-# encoder.fit(train_nona['TypeofContact'])
-
-#학습된 encoder를 사용하여 문자형 변수를 숫자로 변환해줍니다.
-# encoder.transform(train_nona['TypeofContact'])
-# print(train_nona['TypeofContact'])
-
-train_enc = train_nona.copy()
-
-# 모든 문자형 변수에 대해 encoder를 적용합니다.
-for o_col in object_columns:
-    encoder = LabelEncoder()
-    encoder.fit(train_enc[o_col])
-    train_enc[o_col] = encoder.transform(train_enc[o_col])
-
-# 결과를 확인합니다.
-print(train_enc)
-# 결측치 처리
-test = handle_na(test)
-
-# 문자형 변수 전처리
-for o_col in object_columns:
-    encoder = LabelEncoder()
-    
-    # test 데이터를 이용해 encoder를 학습하는 것은 Data Leakage 입니다! 조심!
-    encoder.fit(train_nona[o_col])
-    
-    # test 데이터는 오로지 transform 에서만 사용되어야 합니다.
-    test[o_col] = encoder.transform(test[o_col])
-
-# 결과를 확인
-print(test)
-
-
-# 모델 선언
-from xgboost import XGBClassifier, XGBRegressor
-from catboost import CatBoostClassifier, CatBoostRegressor
-# model = XGBClassifier()
-model = XGBClassifier()
-
-# # 분석할 의미가 없는 칼럼을 제거합니다.
-# train = train_enc.drop(columns=['TypeofContact','Occupation'])
-# test = test.drop(columns=['TypeofContact','Occupation'])
-
-
-# 학습에 사용할 정보와 예측하고자 하는 정보를 분리합니다.
-x = train_enc.drop(columns=['ProdTaken'])
-y = train_enc[['ProdTaken']]
-
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
-print(x_train.shape, x_test.shape, y_train.shape, y_test.shape)
-print(x_train.head())
-print(y_train.head())
-
-
-
-
-#3. 컴파일,훈련
+from sklearn.metrics import r2_score, accuracy_score
+from sklearn.feature_selection import SelectFromModel
+from sklearn.pipeline import make_pipeline
+import matplotlib.pyplot as plt
+import math
 import time
+
+from sklearn.experimental import enable_halving_search_cv
+from sklearn.model_selection import train_test_split, StratifiedKFold,\
+    HalvingRandomSearchCV, RandomizedSearchCV, GridSearchCV
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer, KNNImputer
+
+
+# 1. 데이터
+filepath = './_data/travel/'
+train = pd.read_csv(filepath+'train.csv', index_col=0)
+test = pd.read_csv(filepath+'test.csv', index_col=0)
+
+# print(train.head())
+# print(train.info())
+# print(train.isnull().sum())
+
+# 결측치 TypeofContact 빼고 중간값으로 대체함, 데이터 수치들 보면 중간값이 제일 무난할거 같음--------------------
+train['Age'].fillna(train['Age'].median(), inplace=True)
+train['TypeofContact'].fillna('N', inplace=True) # N으로 채운 이유: 콘택 타입 없는 건 '없음'으로 주고 처리하기 위해
+train['DurationOfPitch'].fillna(train['DurationOfPitch'].median(), inplace=True)
+train['NumberOfFollowups'].fillna(train['NumberOfFollowups'].median(), inplace=True)
+train['PreferredPropertyStar'].fillna(train['PreferredPropertyStar'].median(), inplace=True)
+train['NumberOfTrips'].fillna(train['NumberOfTrips'].median(), inplace=True)
+train['NumberOfChildrenVisiting'].fillna(train['NumberOfChildrenVisiting'].median(), inplace=True)
+train['MonthlyIncome'].fillna(train['MonthlyIncome'].median(), inplace=True)
+
+test['Age'].fillna(test['Age'].median(), inplace=True)
+test['TypeofContact'].fillna('N', inplace=True)
+test['DurationOfPitch'].fillna(test['DurationOfPitch'].median(), inplace=True)
+test['NumberOfFollowups'].fillna(test['NumberOfFollowups'].median(), inplace=True)
+test['PreferredPropertyStar'].fillna(test['PreferredPropertyStar'].median(), inplace=True)
+test['NumberOfTrips'].fillna(test['NumberOfTrips'].median(), inplace=True)
+test['NumberOfChildrenVisiting'].fillna(test['NumberOfChildrenVisiting'].median(), inplace=True)
+test['MonthlyIncome'].fillna(test['MonthlyIncome'].median(), inplace=True)
+# print(train.isnull().sum())
+#-----------------------------------------------------------------------------------------------------------
+
+# object타입 라벨인코딩--------------------
+le = LabelEncoder()
+idxarr = train.columns
+idxarr = np.array(idxarr)
+
+for i in idxarr:
+      if train[i].dtype == 'object':
+        train[i] = le.fit_transform(train[i])
+        test[i] = le.fit_transform(test[i])
+# print(train.info())
+# ------------------------------------------
+
+# 피처임포턴스 그래프 보기 위해 데이터프레임형태의 x_, y_ 놔둠 / 훈련용 넘파이어레이형태의 x, y 생성-----------
+# x_ = train.drop(['ProdTaken','NumberOfChildrenVisiting','NumberOfPersonVisiting','OwnCar'], axis=1) # 피처임포턴스로 확인한 중요도 낮은 탑3 제거
+x_ = train.drop(['ProdTaken','NumberOfChildrenVisiting','NumberOfPersonVisiting','OwnCar', 'MonthlyIncome', 'Designation'], axis=1)
+# x_ = train.drop(['ProdTaken'], axis=1)
+y_ = train['ProdTaken']
+x = np.array(x_)
+y = np.array(y_)
+y = y.reshape(-1, 1) # y값 reshape 해야되서 x도 넘파이로 바꿔 훈련하는 것
+
+# test = test.drop(['NumberOfChildrenVisiting','NumberOfPersonVisiting','OwnCar'], axis=1) # 피처임포턴스로 확인한 중요도 낮은 탑3 제거
+test = test.drop(['NumberOfChildrenVisiting','NumberOfPersonVisiting','OwnCar', 'MonthlyIncome','Designation'], axis=1)
+test = np.array(test)
+# print(x.shape, y.shape)
+#-----------------------------------------------------------------------------------------------------------
+
+''' 이상치 그래프로 확인
+def outliers(data_out):
+    quartile_1, q2, quartile_3 = np.percentile(data_out, [25, 50, 75])
+    print('1사분위: ', quartile_1)
+    print('q2: ', q2)
+    print('3사분위: ', quartile_3)
+    iqr = quartile_3-quartile_1 # interquartile range
+    lower_bound = quartile_1 - (iqr * 1.5)
+    upper_bound = quartile_3 + (iqr * 1.5)
+    print(upper_bound)
+    return np.where((data_out>upper_bound) | (data_out<lower_bound))
+def outliers_printer(dataset):
+    plt.figure(figsize=(10,13))
+    for i in range(dataset.shape[1]):
+        col = dataset[:, i]
+        outliers_loc = outliers(col)
+        print(i, '열의 이상치의 위치: ', outliers_loc, '\n')
+        plt.subplot(math.ceil(dataset.shape[1]/2),2,i+1)
+        plt.boxplot(col)
+        plt.title(i)
+        
+    plt.show()
+# outliers_printer(x)
+#------------------------------------------------------------------
+'''
+
+a3 =  [  89,  110,  120,  121,  137,  147,  172,  210,  211,  247,  310,
+        365,  397,  398,  426,  448,  450,  567,  755,  802,  865,  935,
+       1033, 1036, 1100, 1136, 1237, 1241, 1387, 1413, 1435, 1447, 1449,
+       1461, 1471, 1521, 1540, 1652, 1665, 1722, 1782, 1919, 1936, 1947]
+
+a6 = [   8,   10,   13,   33,   40,   53,   80,   85,  108,  127,  129,
+        150,  151,  172,  178,  199,  206,  214,  233,  254,  256,  281,
+        327,  335,  344,  355,  364,  367,  380,  420,  426,  438,  462,
+        496,  498,  512,  575,  610,  631,  689,  711,  714,  718,  726,
+        757,  761,  768,  777,  778,  790,  816,  827,  829,  834,  840,
+        857,  871,  875,  904,  913,  946,  960,  971,  972,  994, 1007,
+       1009, 1021, 1024, 1060, 1077, 1081, 1098, 1099, 1121, 1124, 1142,
+       1182, 1208, 1237, 1245, 1259, 1289, 1296, 1314, 1322, 1337, 1344,
+       1348, 1362, 1365, 1377, 1389, 1400, 1422, 1436, 1473, 1505, 1558,
+       1591, 1598, 1606, 1610, 1621, 1631, 1641, 1677, 1715, 1724, 1726,
+       1750, 1766, 1817, 1825, 1852, 1858, 1873, 1881, 1887, 1925, 1930]
+
+a10 = [  90,  106,  129,  161,  177,  248,  302,  303,  316,  348,  425,
+        552,  623,  698,  769,  873,  927,  987, 1055, 1076, 1175, 1325,
+       1360, 1404, 1425, 1514, 1525, 1598, 1611, 1663, 1731, 1749, 1753,
+       1768, 1828, 1844, 1853, 1933]
+
+a13 = [  14,   59,   93,  105,  142,  167,  187,  203,  209,  218,  230,
+        250,  265,  311,  314,  322,  342,  378,  447,  479,  503,  536,
+        570,  592,  643,  662,  727,  729,  749,  768,  817,  819,  827,
+        851,  852,  869,  912,  917,  944,  964,  995, 1020, 1097, 1101,
+       1110, 1121, 1123, 1156, 1157, 1203, 1277, 1311, 1316, 1336, 1355,
+       1363, 1380, 1398, 1417, 1474, 1486, 1502, 1507, 1518, 1526, 1536,
+       1561, 1571, 1576, 1578, 1597, 1640, 1643, 1668, 1676, 1716, 1719,
+       1739, 1750, 1783, 1791, 1818, 1822, 1823, 1856, 1870, 1887, 1899,
+       1927]
+
+for i in range(len(a3)): # DurationOfPitch
+    x[a3[i]][3] = 20
+
+# x[485][4] = np.nan # Occupation
+
+# for i in range(len(a6)): # ProductPitched
+#     x[a6[i]][6] = np.nan
+    
+for i in range(len(a10)): # NumberOfTrips
+    x[a10[i]][10] = 5.7
+
+# for i in range(len(a13)): # Designation
+#     x[a13[i]][13] = np.nan
+    
+# outliers_printer(x)
+'''
+x = pd.DataFrame(x, columns=[x_.columns])
+# x['Occupation'].fillna(x['Occupation'].median, inplace=True)
+x['ProductPitched'].fillna(x['ProductPitched'].median, inplace=True)
+# x['Designation'].fillna(x['Designation'].median, inplace=True)
+x['NumberOfTrips'].fillna(x['NumberOfTrips'].median, inplace=True)
+ipt = IterativeImputer(max_iter = 100, random_state = 999)
+# ipt = KNNImputer()
+x = ipt.fit_transform(x)
+x = np.array(x)
+'''
+
+''' PCA 반복문 - XGB 테스트 // 별로 안좋게 나옴, LDA는 y라벨 두개뿐이라 큰 의미 없음
+for i in range(x.shape[1]):
+    pca = PCA(n_components=i+1)
+    x2 = pca.fit_transform(x)
+    x_train, x_test, y_train, y_test = train_test_split(x2, y, train_size=0.8, random_state=123, shuffle=True)
+    model = XGBClassifier(tree_method='gpu_hist', predictor='gpu_predictor', gpu_id=0)
+    model.fit(x_train, y_train)
+    results = model.score(x_test, y_test)
+    print(i+1, '의 결과: ', results)
+#------------------------------------------------------------------
+'''
+
+parameters_xgb = {
+            'n_estimators':[50,100,200,300,400,500],
+            'learning_rate':[0.1,0.2,0.3,0.5,1,0.01,0.001],
+            'max_depth':[None,2,3,4,5,6,7,8,9,10],
+            'gamma':[0,1,2,3,4,5,7,10,100],
+            'min_child_weight':[0,0.1,0.001,0.5,1,5,10,100],
+            'subsample':[0,0.1,0.2,0.3,0.5,0.7,1],
+            'reg_alpha':[0,0.1,0.01,0.001,1,2,10],
+            'reg_lambda':[0,0.1,0.01,0.001,1,2,10],
+              } 
+
+parameters_rnf = [
+    {'n_estimators':[100,200,300]},
+    {'max_depth':[None,6,8,10,12]},
+    {'min_samples_leaf':[3,5,7,10,11,13]},
+    {'min_samples_split':[2,3,5,7,10]},
+    {'n_jobs':[-1]}
+]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state=999, shuffle=True)
+# print(np.unique(y_train, return_counts=True))
+
+# # 2. 모델
+# xgb = XGBRFClassifier(tree_method='gpu_hist', predictor='gpu_predictor', gpu_id=0)
+# rnf = RandomForestClassifier(random_state=678) # 704 : 0.9053708439897699 / Designation 드랍하면 678 : 0.9053708439897699
+# # model = xgb
+# # model = rnf
+# # model = make_pipeline(MinMaxScaler(), HalvingRandomSearchCV(xgb, parameters_xgb, cv=5, n_jobs=-1, verbose=2))
+# model = make_pipeline(MinMaxScaler(), HalvingRandomSearchCV(rnf, parameters_rnf, cv=6, n_jobs=-1, verbose=2, random_state=999))
+# # model = make_pipeline(MinMaxScaler(), GridSearchCV(rnf, parameters_rnf, cv=5, n_jobs=-1, verbose=2))
+# # model = make_pipeline(MinMaxScaler(), xgb)
+# # model = make_pipeline(MinMaxScaler(), rnf)
+
+# 3. 훈련
+# Feature importances 확인
+# def plot_feature_importances(model):
+#     plt.figure(figsize=(13,8))
+#     n_features = x_.shape[1]
+#     plt.barh(np.arange(n_features), model.feature_importances_, align='center')
+#                 # x                     y
+#     plt.yticks(np.arange(n_features), x_.columns) # 눈금 설정
+#     plt.xlabel('Feature Importances')
+#     plt.ylabel('Features')
+#     plt.ylim(-1, n_features) # ylimit : 축의 한계치 설정
+#     if str(models[i]).startswith('XGBClassifier'):
+#         plt.title('XGB()')
+#     else:
+#         plt.title(model)
+# plot_feature_importances(model)
+# plt.show()
+# # NumberOfChildrenVisiting, NumberOfPersonVisiting, OwnCar 가장 영향력 적음
+# # ----------------------------------------------------------------------
+
+
+# import joblib
+# joblib.dump(model,'D:\study_home\_data\dacon_travel\_dat/m360_travel6.dat')
+# # model = joblib.load('D:\study_home\_data\dacon_travel\_dat/m360_travel.dat')
+
+
+# 2. 모델
+
+#----------------------------모델 셀렉션----------------------------------------------------------
+model = RandomForestClassifier(n_estimators=100, random_state=1234)
+model.fit(x_train, y_train)
+print('테스트 스코어: ', model.score(x_test, y_test))
+score = accuracy_score(y_test, model.predict(x_test))
+print('score 결과: ', score)
+print(model.feature_importances_)
+thresholds = model.feature_importances_
+print('-----------------------------------------------')
+bscore = 0
+idx_ = 0
+for i in range(len(thresholds)):
+    selection = SelectFromModel(model, threshold=thresholds[i], prefit=True)
+    select_x_train = selection.transform(x_train)
+    select_x_test = selection.transform(x_test)
+    print(select_x_train.shape, select_x_train.shape)
+    
+    selection_model = XGBClassifier(n_estimators=100,
+              learning_rate=1,
+              max_depth=2,
+              gamma=0,
+              min_child_weight=1,
+              subsample=1,
+              colsample_bytree=0.5,
+              colsample_bylevel=1,
+              colsample_bynode=1,
+              reg_alpha=0.01,
+              tree_method='gpu_hist', predictor='gpu_predictor', gpu_id=0, random_state=1234,
+              )
+    
+    selection_model.fit(select_x_train, y_train)
+    
+    y_predict = selection_model.predict(select_x_test)
+    score = accuracy_score(y_test, y_predict)
+    print('Thresh=%.3f, n=%d, R2: %.2f%%'%(thresholds[i], select_x_train.shape[1], score*100), '\n')
+    if score >= bscore:
+        bscore = score
+        idx_=i
+f_to_drop = []
+for i in range(len(thresholds)):
+    if thresholds[idx_]>=thresholds[i]:
+        f_to_drop.append(i)
+        
+print(f_to_drop)
+# [0, 7, 8, 11, 15, 16, 22, 24, 31, 32, 39, 40, 47, 48, 55, 56, 57]
+xaf_train = np.delete(x_train, f_to_drop, axis=1)
+xaf_test = np.delete(x_test, f_to_drop, axis=1)
+model.fit(xaf_train, y_train)
+print('드랍 후 테스트 스코어: ', model.score(xaf_test, y_test))
+score = accuracy_score(y_test, model.predict(xaf_test))
+print('드랍 후 acc_score 결과: ', score)
+#-------------------------------------------------------------------------------------------------------------
+
+
 start = time.time()
-model.fit(x_train, y_train)  # **fit_params
-end = time.time()- start
-#4. 평가, 예측
-result = model.score(x_test, y_test)
+model.fit(x_train, y_train)
+end = time.time()
 
-print('model.score : ', result) # model.score :  1.0
+# 4. 평가, 예측
+results = model.score(x_test, y_test)
+print('스코어: ', results)
+print('걸린 시간: ', end-start)
 
-y_predict = model.predict(x_test)
-print('accuracy_score :',accuracy_score(y_test,y_predict))
+# 5. 제출 준비
+y_submit = model.predict(test)
+
+submission = pd.read_csv(filepath+'sample_submission.csv', index_col=0)
+submission['ProdTaken'] = y_submit
+submission.to_csv(filepath + 'submission.csv', index = True)
 
 
 
-pred = model.predict(test)
-y_summit = [1 if x > 0.5 else 0 for x in pred]
-
-submission_set = pd.read_csv(path + 'sample_submission.csv', # + 명령어는 문자를 앞문자와 더해줌
-                             index_col=0) # index_col=n n번째 컬럼을 인덱스로 인식
-
-submission_set['ProdTaken'] = y_summit
-
-submission_set.to_csv(path + 'sample_submission_cat_drp.csv', index = True)
+# submission 1번파일
