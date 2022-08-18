913345301f379da6719cb02f6224c2e0a0d068c2
wofvh <wofvh@naver.com>
2022-08-18 20:40:14 +0900

    08/18

--- ml/m50_Voting2_삼대장12_kaggle_house.py
+++ ml/m50_Voting2_삼대장12_kaggle_house.py
@@ -1,167 +1,36 @@
+from json import load
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import KFold,cross_val_score,GridSearchCV,StratifiedKFold, RandomizedSearchCV
+from sklearn.datasets import load_breast_cancer, load_boston, fetch_california_housing
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from sklearn.metrics import accuracy_score, r2_score
 from sklearn.model_selection import train_test_split
-import warnings
-warnings.filterwarnings(action="ignore")
+from sklearn.metrics import accuracy_score, r2_score
 
-parameters = [
-    {'XGB__n_estimators' : [100,200,300,400,500],
-    'XGB__learning_rate' : [0.01,0.05,0.1,0.15],
-    'XGB__max_depth' : [3,5,7,10,15],
-    'XGB__gamma' : [0,1,2,3],
-    'XGB__colsample_bytree' : [0.8,0.9]}]
+# 1. 데이터
+path = './_data/ddarung/'
+train_set = pd.read_csv(path+'train.csv',index_col=0) # index_col = n : n번째 칼럼을 인덱스로 인식
+# print(train_set)
+# print(train_set.shape) # (1459, 10)
 
-parameters_rfr = [{
-    'RFR__bootstrap': [True], 'RFR__max_depth': [5, 10, None], 
-    'RFR__max_features': ['auto', 'log2'], 'RFR__n_estimators': [5, 6, 7, 8, 9, 10, 11, 12, 13, 15]}]
+test_set = pd.read_csv(path+'test.csv', index_col=0)
+# print(test_set)
+# print(test_set.shape) # (715, 9)
 
-kfold = StratifiedKFold(n_splits=5,shuffle=True,random_state=100)
+### 결측치 처리(일단 제거로 처리) ###
+print(train_set.info())
+print(train_set.isnull().sum()) # 결측치 전부 더함
+# train_set = train_set.dropna() # nan 값(결측치) 열 없앰
+train_set = train_set.fillna(0) # 결측치 0으로 채움
+print(train_set.isnull().sum()) # 없어졌는지 재확인
 
+x = train_set.drop(['count'], axis=1) # axis = 0은 열방향으로 쭉 한줄(가로로 쭉), 1은 행방향으로 쭉 한줄(세로로 쭉)
+y = train_set['count']
 
-#1. 데이터
-path = './_data/kaggle_house/'
-train_set = pd.read_csv(path + 'train.csv') # + 명령어는 문자를 앞문자와 더해줌  index_col=n n번째 컬럼을 인덱스로 인식
-            
-test_set = pd.read_csv(path + 'test.csv') # 예측에서 쓸거임  3
+print(x.shape, y.shape) # (1328, 9) (1328,)
 
-
-# 수치형 변수와 범주형 변수 찾기
-numerical_feats = train_set.dtypes[train_set.dtypes != "object"].index
-categorical_feats = train_set.dtypes[train_set.dtypes == "object"].index
-numerical_feats_ = test_set.dtypes[test_set.dtypes != "object"].index
-categorical_feats_ = test_set.dtypes[test_set.dtypes == "object"].index
-
-#################긁어온거####################################
-
-num_strong_corr = ['SalePrice','OverallQual','TotalBsmtSF','GrLivArea','GarageCars',
-                   'FullBath','YearBuilt','YearRemodAdd']
-
-num_weak_corr = ['MSSubClass', 'LotFrontage', 'LotArea', 'OverallCond', 'MasVnrArea', 'BsmtFinSF1',
-                 'BsmtFinSF2', 'BsmtUnfSF', '1stFlrSF', '2ndFlrSF','LowQualFinSF', 'BsmtFullBath',
-                 'BsmtHalfBath', 'HalfBath', 'BedroomAbvGr', 'KitchenAbvGr', 'TotRmsAbvGrd',
-                 'Fireplaces', 'GarageYrBlt', 'GarageArea', 'WoodDeckSF','OpenPorchSF',
-                 'EnclosedPorch', '3SsnPorch', 'ScreenPorch', 'PoolArea', 'MiscVal', 'MoSold', 'YrSold']
-
-catg_strong_corr = ['MSZoning', 'Neighborhood', 'Condition2', 'MasVnrType', 'ExterQual',
-                    'BsmtQual','CentralAir', 'Electrical', 'KitchenQual', 'SaleType']
-
-catg_weak_corr = ['Street', 'Alley', 'LotShape', 'LandContour', 'Utilities', 'LotConfig', 
-                  'LandSlope', 'Condition1',  'BldgType', 'HouseStyle', 'RoofStyle', 
-                  'RoofMatl', 'Exterior1st', 'Exterior2nd', 'ExterCond', 'Foundation', 
-                  'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2', 'Heating', 
-                  'HeatingQC', 'Functional', 'FireplaceQu', 'GarageType', 'GarageFinish', 
-                  'GarageQual', 'GarageCond', 'PavedDrive', 'PoolQC', 'Fence', 'MiscFeature', 
-                  'SaleCondition' ]
-
-cols_fillna = ['PoolQC','MiscFeature','Alley','Fence','MasVnrType','FireplaceQu',
-               'GarageQual','GarageCond','GarageFinish','GarageType', 'Electrical',
-               'KitchenQual', 'SaleType', 'Functional', 'Exterior2nd', 'Exterior1st',
-               'BsmtExposure','BsmtCond','BsmtQual','BsmtFinType1','BsmtFinType2',
-               'MSZoning', 'Utilities']
-
-for col in cols_fillna : 
-    train_set[col].fillna('None', inplace=True)
-    test_set[col].fillna('None', inplace=True)
-
-total = train_set.isnull().sum().sort_values(ascending=False)
-percent = (train_set.isnull().sum()/train_set.isnull().count()).sort_values(ascending=False)
-missing_data = pd.concat([total, percent], axis=1, keys=['Total','Percent'])
-
-train_set.fillna(train_set.mean(), inplace=True)
-test_set.fillna(test_set.mean(), inplace=True)
-
-total = train_set.isnull().sum().sort_values(ascending=False)
-percent = (train_set.isnull().sum()/train_set.isnull().count()).sort_values(ascending=False)
-missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
-
-print(train_set.isnull().sum().sum(), test_set.isnull().sum().sum()) # 0 0 출력시 결측치 확인 끝
-
-id_test = test_set['Id']
-
-to_drop_num = num_weak_corr
-to_drop_catg = catg_weak_corr
-
-cols_to_drop = ['Id'] + to_drop_num + to_drop_catg
-
-for df in [train_set, test_set] :
-    df.drop(cols_to_drop, inplace=True, axis = 1)
-    
-# 'MSZoning'
-msz_catg2 = ['RM', 'RH']
-msz_catg3 = ['RL', 'FV'] 
-
-# Neighborhood
-nbhd_catg2 = ['Blmngtn', 'ClearCr', 'CollgCr', 'Crawfor', 'Gilbert', 'NWAmes', 'Somerst', 'Timber', 'Veenker']
-nbhd_catg3 = ['NoRidge', 'NridgHt', 'StoneBr']
-
-# Condition2
-cond2_catg2 = ['Norm', 'RRAe']
-cond2_catg3 = ['PosA', 'PosN'] 
-
-# SaleType
-SlTy_catg1 = ['Oth']
-SlTy_catg3 = ['CWD']
-SlTy_catg4 = ['New', 'Con']
-
-for df in [train_set, test_set]:
-    
-    df['MSZ_num'] = 1  
-    df.loc[(df['MSZoning'].isin(msz_catg2) ), 'MSZ_num'] = 2    
-    df.loc[(df['MSZoning'].isin(msz_catg3) ), 'MSZ_num'] = 3        
-    
-    df['NbHd_num'] = 1       
-    df.loc[(df['Neighborhood'].isin(nbhd_catg2) ), 'NbHd_num'] = 2    
-    df.loc[(df['Neighborhood'].isin(nbhd_catg3) ), 'NbHd_num'] = 3    
-
-    df['Cond2_num'] = 1       
-    df.loc[(df['Condition2'].isin(cond2_catg2) ), 'Cond2_num'] = 2    
-    df.loc[(df['Condition2'].isin(cond2_catg3) ), 'Cond2_num'] = 3    
-    
-    df['Mas_num'] = 1       
-    df.loc[(df['MasVnrType'] == 'Stone' ), 'Mas_num'] = 2 
-    
-    df['ExtQ_num'] = 1       
-    df.loc[(df['ExterQual'] == 'TA' ), 'ExtQ_num'] = 2     
-    df.loc[(df['ExterQual'] == 'Gd' ), 'ExtQ_num'] = 3     
-    df.loc[(df['ExterQual'] == 'Ex' ), 'ExtQ_num'] = 4     
-   
-    df['BsQ_num'] = 1          
-    df.loc[(df['BsmtQual'] == 'Gd' ), 'BsQ_num'] = 2     
-    df.loc[(df['BsmtQual'] == 'Ex' ), 'BsQ_num'] = 3     
- 
-    df['CA_num'] = 0          
-    df.loc[(df['CentralAir'] == 'Y' ), 'CA_num'] = 1    
-
-    df['Elc_num'] = 1       
-    df.loc[(df['Electrical'] == 'SBrkr' ), 'Elc_num'] = 2 
-
-
-    df['KiQ_num'] = 1       
-    df.loc[(df['KitchenQual'] == 'TA' ), 'KiQ_num'] = 2     
-    df.loc[(df['KitchenQual'] == 'Gd' ), 'KiQ_num'] = 3     
-    df.loc[(df['KitchenQual'] == 'Ex' ), 'KiQ_num'] = 4      
-    
-    df['SlTy_num'] = 2       
-    df.loc[(df['SaleType'].isin(SlTy_catg1) ), 'SlTy_num'] = 1  
-    df.loc[(df['SaleType'].isin(SlTy_catg3) ), 'SlTy_num'] = 3  
-    df.loc[(df['SaleType'].isin(SlTy_catg4) ), 'SlTy_num'] = 4 
-
-train_set.drop(['MSZoning','Neighborhood' , 'Condition2', 'MasVnrType', 'ExterQual', 'BsmtQual','CentralAir', 'Electrical', 'KitchenQual', 'SaleType', 'Cond2_num', 'Mas_num', 'CA_num', 'Elc_num', 'SlTy_num'], axis = 1, inplace = True)
-test_set.drop(['MSZoning', 'Neighborhood' , 'Condition2', 'MasVnrType', 'ExterQual', 'BsmtQual','CentralAir', 'Electrical', 'KitchenQual', 'SaleType', 'Cond2_num', 'Mas_num', 'CA_num', 'Elc_num', 'SlTy_num'], axis = 1, inplace = True)
-
-
-##############################긁어온거끝################################
-
-x = train_set.drop(['SalePrice'], axis=1)
-y = train_set['SalePrice']
-
-x_train, x_test, y_train, y_test = train_test_split(x,y,
-                                                    train_size=0.75,
-                                                    random_state=984
-                                                    )
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, shuffle=True, random_state=123, train_size=0.8
+    )
 
 Scaler = StandardScaler() #Bagging 할때 스케일러 필수 
 x_train = Scaler.fit_transform(x_train)
@@ -202,4 +71,7 @@ for model in classifier:  #model2는 모델이름 #
     
 print("보팅결과 : ", round(score,4 ))
     
-# XGBClassifier정확도 : 0.9912
+# CatBoostRegressor정확도 : 0.7958
+# XGBRFRegressor정확도 : 0.7610
+# LGBMRegressor정확도 : 0.7679
+# 보팅결과 :  0.7911
