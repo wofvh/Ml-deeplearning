34f5c3cd3cb8e287b8c5338144552e7150a1fc42
wofvh <wofvh@naver.com>
2022-08-24 21:09:41 +0900

    08/24

--- tf114/tf14_02_cancer.py
+++ tf114/tf14_02_cancer.py
@@ -5,7 +5,7 @@ from sklearn import datasets
 import numpy as np
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score,mean_squared_error
+from sklearn.metrics import r2_score, mean_absolute_error, accuracy_score ,mean_squared_error
 tf.set_random_seed(123)
 
 datasets = load_breast_cancer()
@@ -15,79 +15,54 @@ y_data = datasets.target
 
 # sess = tf.compat.v1.InteractiveSession()
 print(x_data.shape,y_data.shape)# x(569, 30) y(569,)
-
-y_data = y_data.reshape(569,1)
+y_data = y_data.reshape(-1,1)
 print(y_data.shape) #(569, 1)
 
 x_train , x_test , y_train , y_test = train_test_split(x_data, y_data, train_size=0.8, shuffle=True, random_state=66, stratify=y_data)
 
-print(type(x_train), type(y_train)) # <class 'numpy.ndarray'> <class 'numpy.ndarray'>
-print(x_train.dtype, y_train.dtype) # (455, 30) (455, 1)
+# print(type(x_train), type(y_train)) # <class 'numpy.ndarray'> <class 'numpy.ndarray'>
+# print(x_train.dtype, y_train.dtype) # (455, 30) (455, 1)
 
-# y_train = np.array(y_train, dtype=np.int32)
+# # y_train = np.array(y_train, dtype=np.int32)
 
 print(x_train.shape, x_test.shape , y_train.shape, y_test.shape)
 #     (455, 30)     (114, 30)      (455, 1)       (114, 1)
 
-x = tf.compat.v1.placeholder(tf.float32, shape=[None, 30])
-y = tf.compat.v1.placeholder(tf.float32, shape=[None, 1])
+x = tf.placeholder(tf.float32, shape=[None, 30])
+y = tf.placeholder(tf.float32, shape=[None,1])
 # w = tf.placeholder(tf.float32, shape=[30,1])
 # b = tf.placeholder(tf.float32, shape=[1])
 w = tf.compat.v1.Variable(tf.compat.v1.zeros([30,1]), name='weight')    # y = x * w  
 b = tf.compat.v1.Variable(tf.compat.v1.zeros([1]), name='bias')   
 
-hypothesis = tf.compat.v1.sigmoid (tf.matmul(x, w) + b) #예
-
-# hypothesis = tf.sigmoid(hypothesis)
-# hypothesis = tf.sigmoid(tf.matmul(x, w) + b)
+hypothesis = tf.compat.v1.sigmoid(tf.compat.v1.matmul(x,w) + b) #예
 
 #3-1. 컴파일
 # loss = tf.reduce_mean(tf.square(hypothesis - y))    # mse
-loss = -tf.reduce_mean(y*tf.log(hypothesis)+(1-y)*tf.log(1-hypothesis))   # binary_crossentropy
+loss = -tf.reduce_mean(y_train*tf.log(hypothesis)+(1-y_train)*tf.log(1-hypothesis))  # binary_crossentropy
 
 # optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-7)
-optimizer = tf.train.AdadeltaOptimizer(learning_rate=0.001)
+optimizer = tf.train.AdamOptimizer(learning_rate= 1e-6)
 train = optimizer.minimize(loss)
 
 #3-2. 훈련
 sess = tf.compat.v1.Session()
 sess.run(tf.compat.v1.global_variables_initializer())
 
-epochs= 50        
+epochs= 2001   
 for step in range(epochs):
      # sess.run(train)
-      train_, loss_val, w_val, b_val = sess.run([train,loss,w,b],
-                                       feed_dict={x_train:x_data, y_train:y_data}) #행렬을 받아서 실행하는 것이 아니라 피드데이터를 받아서 실행한다.
-            # if step %2 == 0:
-            #     print(step, loss_val, w_val, b_val)               
+    loss_val, hy_val, _, w_val, b_val, = sess.run([loss, hypothesis, train, w, b,],
+                                        feed_dict={x:x_train,y:y_train}) #행렬을 받아서 실행하는 것이 아니라 피드데이터를 받아서 실행한다.
+    if step %20 ==0:
+        print(epochs, '\t', 'loss:',loss_val, '\t', hy_val)       
 
 #4. 평가, 예측
-y_predict = tf.cast(hypothesis >=0.5, dtype=tf.int32)  
-y_predict = sess.run([hypothesis], feed_dict={x:x_test, y:y_test})
+# 4. 평가, 예측
+y_pred = sess.run(hypothesis, feed_dict={x: x_test})
+y_pred = np.where(y_pred > 0.5, 1, 0)
 
-# acc = accuracy_score(y_train, np.round(y_predict))
-# print("acc:", acc)
-# mae = mean_squared_error(y_data, hy_val)
-# print("mae", mae)
+acc = accuracy_score(y_test, y_pred)
+print('acc : ', acc)
 
-# sess.close()
-
-# # 3-2. 훈련
-# epochs = 5001
-# for step in range(epochs):
-#     _, hy_val, cost_val, b_val = sess.run([train,hypothesis,loss,b], feed_dict={x:x_train, y:y_train})
-#     if step%20 == 0:
-#         print(step, cost_val, hy_val)
-        
-# print('최종: ', cost_val, hy_val)
-
-# # 4. 평가, 예측
-# predict = tf.cast(hypothesis>=0.5, dtype=tf.float32)
-# y_predict= sess.run([hypothesis], feed_dict={x:x_test, y:y_test})
-# acc = accuracy_score(y_data, np.round(y_predict))
-# print('acc: ', acc)
-
-# mae = mean_squared_error(y_data, hy_val)
-# print('mae: ', mae)
-
-# sess.close()
+sess.close()
--- tf114/tf30_test.py
+++ tf114/tf30_test.py
@@ -1,63 +1,84 @@
-from sklearn import datasets
-from sklearn.datasets import load_diabetes
+from sklearn.datasets import load_breast_cancer
 import tensorflow as tf
+from sklearn.metrics import r2_score, mean_absolute_error, accuracy_score
+from sklearn.model_selection import train_test_split
 import numpy as np
+import pandas as pd
+import numpy
+epsilon=numpy.finfo('float').eps
 tf.set_random_seed(66)
 
-#1. 데이터
-datasets = load_diabetes()
+# 1. 데이터
+sess = tf.compat.v1.Session()
+datasets = load_breast_cancer()
+# x_data = pd.DataFrame(datasets.data, columns=datasets.feature_names)
+# y_data = pd.DataFrame(datasets.target, columns=['target'])
 x_data = datasets.data
 y_data = datasets.target
-print(x_data.shape, y_data.shape)    #(442, 10) (442,)
+y_data = y_data.reshape(-1, 1)
+print(x_data) # (569, 30)
+print(y_data) # (569, 1)
+print(x_data.dtype) # float64
+print(y_data.dtype) # int32
+# change type
+# x_data = x_data.astype('float32')
+# y_data = y_data.astype('float32')
 
-y_data = y_data.reshape(442,1)
-print(y_data.shape)   # (442, 1)
-
-from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x_data, y_data,
-                                                    train_size=0.8, shuffle=True, random_state=66)
-
-print(x_train.shape, y_train.shape)   # (354, 13) (354, 1)
-print(x_test.shape, y_test.shape)     # (152, 13) (152, 1)
-
-x = tf.placeholder(tf.float32, shape=[None, 10])
-y = tf.placeholder(tf.float32, shape=[None, 1])
-w = tf.compat.v1.Variable(tf.random.normal([10,1]), name='weight')    # y = x * w  
-b = tf.compat.v1.Variable(tf.random.normal([1]), name='bias')   
-
-#2. 모델구성
-# hypothesis = x * w + b
-hypothesis = tf.matmul(x, w) + b
-
-#3-1. 컴파일
-loss = tf.reduce_mean(tf.square(hypothesis - y))    # mse
-# loss = -tf.reduce_mean(y*tf.log(hypothesis)+(1-y)*tf.log(1-hypothesis))   # binary_crossentropy
-
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.000995)
-train = optimizer.minimize(loss)
-
-#3-2. 훈련
-sess = tf.compat.v1.Session()
+x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2, random_state=66, stratify=y_data)
 sess.run(tf.compat.v1.global_variables_initializer())
 
-for epochs in range(2001):
-    _, loss_val, w_val= sess.run([train, loss, w], feed_dict={x:x_train, y:y_train})
-    print(epochs, '\t', loss_val, '\t', w_val)
-           
-#4. 예측
-predict = tf.matmul(x, w_val) + b   # predict = model.predict
+x=tf.compat.v1.placeholder(tf.float32, shape=[None,30])
+y=tf.compat.v1.placeholder(tf.float32, shape=[None,1])
 
-y_predict = sess.run(predict, feed_dict={x:x_test, y:y_test})
-print("예측 : " , y_predict)
+w=tf.Variable(tf.compat.v1.random_normal([30,1]), name='weight')
+b=tf.Variable(tf.compat.v1.random_normal([1]), name='bias')
+
+w=tf.compat.v1.random_normal([30,1])
+
+# w=tf.Variable(tf.zeros([30,1]), name='weight')
+# b=tf.Variable(tf.zeros([1]), name='bias')
+
+# w=tf.Variable(tf.ones([30,1]), name='weight')
+# b=tf.Variable(tf.ones([1]), name='bias')
+
+# w dtype change to float64
+# w = tf.cast(w, tf.float64)
+# b = tf.cast(b, tf.float64)
+# print(x_data[0:1])
+# print(x_data[0:1].shape)
+
+
+# print(tf.matmul(x_data[0:1], w))
+# print(sess.run(tf.matmul(x_data[0:1], w)))
+
+
+# 2. 모델구성
+
+
+hypothesis = tf.sigmoid(tf.matmul(x, w) + b)
+loss = -tf.reduce_mean(y * tf.log(hypothesis) + (1 - y) * tf.log(1 - hypothesis))
+# loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=y, logits=tf.matmul(x, w) + b)
+# log1p : log(1+x) 계산
+optimizer = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=0.01)
+# optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=0.01)
+train = optimizer.minimize(loss)
+
+# 3. 훈련
+
+sess = tf.compat.v1.Session()
+sess.run(tf.compat.v1.global_variables_initializer())
+epoch = 2001
+for epochs in range(epoch) :
+    cost_val, hy_val, _, w_val, b_val = sess.run([loss, hypothesis, train, w, b],
+                                   feed_dict={x: x_train, y: y_train})
+    if epochs % 100 == 0 :
+        print(epochs, 'loss : ', cost_val, '\n')
+        
+# 4. 평가, 예측
+y_pred = sess.run(hypothesis, feed_dict={x: x_test})
+y_pred = np.where(y_pred > 0.5, 1, 0)
+
+acc = accuracy_score(y_test, y_pred)
+print('acc : ', acc)
 
 sess.close()
-
-from sklearn.metrics import r2_score, mean_absolute_error
-r2 = r2_score(y_test, y_predict)
-print('r2스코어 : ', r2)
-
-mae = mean_absolute_error(y_test, y_predict)
-print('mae : ', mae)
-
-# r2스코어 :  0.02105369681810465
-# mae :  68.48492225904143
