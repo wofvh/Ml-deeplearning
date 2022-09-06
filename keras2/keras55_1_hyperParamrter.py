9340f1b92888f0b40b5eafcf87f0014232280c99
wofvh <wofvh@naver.com>
2022-09-06 10:13:28 +0900

    09/05

---
+++ keras2/keras55_1_hyperParamrter.py
@@ -0,0 +1,6 @@
+import numpy as np
+from keras.datasets import mnist
+from tensorflow.python.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D,Input
+from tensorflow.python.keras.models import sequential
+
+#1. 데이터
