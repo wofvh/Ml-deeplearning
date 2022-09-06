ac968a9a231c5fb526d4108ee43b1f31e829aa1a
wofvh <wofvh@naver.com>
2022-09-06 10:16:26 +0900

    09/05

--- keras2/keras55_1_hyperParamrter.py
+++ keras2/keras55_1_hyperParamrter.py
@@ -2,5 +2,9 @@ import numpy as np
 from keras.datasets import mnist
 from tensorflow.python.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D,Input
 from tensorflow.python.keras.models import sequential
+import tensorflow as tf
+print(tf.__version__)
 
 #1. 데이터
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
