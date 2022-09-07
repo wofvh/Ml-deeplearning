97c1241e30fe612ce4326033ae179c2c2cd92b5a
wofvh <wofvh@naver.com>
2022-09-07 15:28:19 +0900

    09/07

--- keras2/keras65_1_Vgg_16_include.top.py
+++ keras2/keras65_1_Vgg_16_include.top.py
@@ -3,4 +3,9 @@ from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense
 from keras.applications import VGG16
 
-model = VGG16()
+model = VGG16() # vgg16은 이미지 분류 모델 
+model = VGG16(weights='imagenet', include_top=True, input_shape=(32,32,3)) # include_top=False : 이미지 분류 모델에서는 1000개의 노드가 있지만 우리는 10개의 노드만 필요하므로 False로 설정
+
+model.summary()
+print(len(model.weights))               #32
+print(len(model.trainable_weights))     #32 #trainable_weights는 훈련가능한 가중치 #trainable_weights
