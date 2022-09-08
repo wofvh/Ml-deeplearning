b75013ef5a83ed70749c5061b0a3ba8f49ff170a
wofvh <wofvh@naver.com>
2022-09-08 17:58:57 +0900

    09/08

---
+++ keras2/keras69_cifar100_04_ResNet101_.py
@@ -0,0 +1,30 @@
+from threading import activeCount
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, GlobalAveragePooling2D, Dropout
+from keras.datasets import cifar100
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau
+from keras.utils import to_categorical
+from keras.applications import ResNet50,ResNet101
+from keras.applications.vgg19 import preprocess_input, decode_predictions
+import time
+
+(x_train, y_train), (x_test, y_test) = cifar100.load_data()
+
+X_train = preprocess_input(x_train)
+x_test = preprocess_input(x_test)
+
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+resnet101 = ResNet101(weights='imagenet', include_top=False, input_shape=(32,32,3))
+resnet101._trainable = False
+
+model = Sequential()
+model.add(resnet101)
+model.add(GlobalAveragePooling2D())
+model.add(Dense(256, activation= 'relu'))
+model.add(Dropout(0.2))
+model.add(Dense(100,activation='sodftmax'))
+
+
+model.compile
---
+++ keras2/keras70_01_cat_dog.py
@@ -0,0 +1,77 @@
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing import image
+from keras.models import Sequential
+from keras.applications import ResNet101V2
+from keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPool2D, GlobalAvgPool2D
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau
+from keras.optimizers import Adam
+import time
+
+train_datagen = ImageDataGenerator(
+                rescale=1./255, 
+                validation_split=0.2)
+
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+xy_train = train_datagen.flow_from_directory(
+    'D:/study_data/_data/image/cat_dog/train_set',
+    target_size = (50, 50),
+    batch_size = 32,
+    class_mode = 'binary',
+    subset='training',
+    shuffle = True)
+
+xy_val = train_datagen.flow_from_directory(
+    'D:/study_data/_data/image/cat_dog/train_set',
+    target_size = (50, 50),
+    batch_size = 32,
+    class_mode = 'binary',
+    subset = 'validation')   
+
+xy_test = test_datagen.flow_from_directory(
+    'D:/study_data/_data/image/cat_dog/test_set',
+    target_size= (50, 50),
+    batch_size = 32,
+    class_mode = 'binary')
+
+#2. 모델
+pre_train = ResNet101V2(weights='imagenet', include_top=False,
+              input_shape=(50, 50, 3))
+pre_train.trainable = True
+
+model = Sequential()
+model.add(pre_train)
+model.add(GlobalAvgPool2D())
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(1, activation='sigmoid'))
+
+#3. 컴파일 
+learning_rate = 1e-4
+optimizer = Adam(learning_rate=learning_rate)
+
+model.compile(loss = "binary_crossentropy", optimizer=optimizer, metrics=['acc'])
+es = EarlyStopping(monitor='val_loss', patience=10, mode='min', restore_best_weights=True)
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', patience=5, mode='amin', verbose=1, factor=0.5)
+
+start = time.time()
+hist = model.fit(xy_train, epochs=100, steps_per_epoch=20, validation_data=xy_val, validation_steps=40, callbacks=[es, reduce_lr]) 
+end = time.time() - start
+
+#4. 예측
+loss = model.evaluate(xy_test)
+print("걸린 시간 : ", round(end, 2))
+print("loss, acc : ", loss)
+
+'''
+걸린 시간 :  165.28
+loss, acc :  [0.4821479618549347, 0.8042511343955994]
+'''
+
+
+# 걸린 시간 :  328.9
+# loss, acc :  [3.810443013207987e-05, 1.0]
