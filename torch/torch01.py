e59088b0ed8417c7bdcb5181343f1ce7be18bc86
wofvh <wofvh@naver.com>
2022-09-14 17:41:29 +0900

    09/14

--- torch/torch01.py
+++ torch/torch01.py
@@ -1,70 +1,63 @@
+from pickletools import optimize
+from unittest import result
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 
-#1. 데이터
 x = np.array([1,2,3])
 y = np.array([1,2,3])
 
-#토치에서 1차원으로 데이터 사용하면 오류날 수도 있음, 2차원으로 reshape
-#정수 tensor 형태
-x = torch.FloatTensor(x).unsqueeze(1)   #(3,) => (3,1) (방법1)
-y = torch.FloatTensor(y).unsqueeze(-1)  #(3,) => (3,1) (방법2)
+x  = torch.FloatTensor(x).unsqueeze(1)
+y = torch.FloatTensor(y).unsqueeze(-1)
 
 print(x,y)
-print(x.shape, y.shape)
+print(x.shape,y.shape)
 
 #2. 모델구성
 # model = Sequential()
-model = nn.Linear(1,1) #input x, output y / 단층레이어, 선형회귀
+model = nn.Linear(1,1)#input x, output y / 단층레이어, 선형회귀
 
-#3. 컴파일, 훈련
-# model.compile(loss='mes', optimizer='SGD')
-criterion = nn.MSELoss() #표준
-optimizer = optim.SGD(model.parameters(), lr=0.01) #옵티마이저는 모든 파라미터에서 연산되기 때문에 model.parameters 명시
-#optim.Adam(model.parameters(), lr=0.01)
+#3. 컴파일 훈련
+criterion  = nn.MSELoss()# 로스는 표준 , mse
+optimizer = optim.SGD(model.parameters(), lr=0.01)#Parameter를 모두 업데이트 하겠다. lr=0.01 <<무조건 명시해야함
 
-def train(model,  criterion, optimizer, x, y):
-    # model. train()            # 훈련모드 (생략할 수도 있음)
-    optimizer.zero_grad()   #손실함수 기울기 초기화
+def train(model, criterion, optimizer, x, y):
+    optimizer.zero_grad() #돌면서 미분해서 남은 값을을 0 으로 초기화#역전파 전에 gradient를 0으로 초기화
     
-    hypothesis = model(x)
+    hypothesis = model(x) #모델에 x를 넣어서 예측값을 도출 #y = wx + b 손실함수 계산을 위해 예측값을 도출
     
-    loss = criterion(hypothesis, y) #mse(hypothesis - y) = loss 
+    loss = criterion(hypothesis, y) #예측값과 실제값을 비교해서 손실값을 도출 #손실값을 계산
     
-    loss.backward()
-    optimizer.step()
-    return loss.item()
+    loss.backward()     #손실값을 기준으로 역전파를 수행 #역전파를 수행 #손실값을 기준으로 역전파를 수행
+    optimizer.step()    #업데이트 #업데이트
+    return loss.item()  #loss.item()은 loss의 값을 스칼라 값으로 도출 
 
-epochs = 700
-for epoch in range(1, epochs+1) :
-        loss = train(model, criterion, optimizer, x , y)
-        print("epoch : {}, loss: {}".format(epoch, loss))
+epochs = 500
+for epoch in range(1 , epochs +1):
+    loss = train(model, criterion , optimizer, x, y)
+    print("epoch : {}, loss : {}".format(epoch, loss)) 
+    
 '''
 파이토치 반복 문법
     1.optimizer.zero_grad()   #손실함수 기울기 초기화
     2.loss.backward() 역전파
     3.optimizer.step 역전파를 하면서 웨이트 갱신
     1 epoch = 1-> 2-> 3
-'''    
 
-#4. 평가, 예측
-# loss = model.evaluate(x, y)
+'''
+def evaluate(model , criterion , x,y):
+    model.eval() #모델을 평가 모드로 전환 #평가모드 (반드시 명시해야함)
+    
+    with torch.no_grad():
+        x_predict = model(x)
+        results = criterion(x_predict, y)
+    return results.item()
 
-# 평가는 만들어진 가중치로만 평가하기 때문에 가중치 갱신할 필요 없음 => optimizer 안들어가도 됨
-def evaluate(model, criterion, x, y ):
-        model.eval()        #평가모드 (반드시 명시 必)
-        
-        with torch.no_grad():   
-            x_predict = model(x)
-            results = criterion(x_predict, y)
-        return results.item()
+loss2 = evaluate(model, criterion, x,y)
+print("최종 loss :", loss2)
 
-loss2 = evaluate(model, criterion, x, y)    
-print("최종 loss : ", loss2)
+results = model(torch.tensor([[4.0]])) #[[4.0]] 2차원으로 만들어줘야함 예) [[[]]] 3차원
+print('4의 예측값:', results.item())
 
-#y_predict = model.predict([4])    
-results = model(torch.Tensor([[4]])) #shape 맞춰주기
-print('4의 예측값 : ',results.item())
