d46a7ca5cbee584d4ae4df56a11fb1e706c2aeeb
wofvh <wofvh@naver.com>
2022-09-16 10:41:47 +0900

    09/15

--- m102_for_Feil.py
+++ m102_for_Feil.py
@@ -1,14 +1,14 @@
 
 
 
-# feil_name = ["01_iris","02_cancer","03_diabets","04_wine","05_fetch_covtype",\
-#     "06_digits","07_kaggle_titanic","08_boston.py","09_california","10_ddarung",\
-#         "11_kaggle_bike","12_kaggle_house","13_mnist","14_cifar10",]
+feil_name = ["01_iris","02_cancer","03_diabets","04_wine","05_fetch_covtype",\
+    "06_digits","07_kaggle_titanic","08_boston.py","09_california","10_ddarung",\
+        "11_kaggle_bike","12_kaggle_house","13_mnist","14_cifar10",]
 
 # keras69_01_cifar100_VGG19.py
 
-feil_name = ["01_cifar100","02_Xception","03_ResNet50","04_ResNet101","05_InceptionV3","06_InceptionResNetV2",\
-    "07_DenseNet121","08_MobileNetV2","09_NASNetMobile","10_EfficientNetB0"]
+# feil_name = ["01_cifar100","02_Xception","03_ResNet50","04_ResNet101","05_InceptionV3","06_InceptionResNetV2",\
+#     "07_DenseNet121","08_MobileNetV2","09_NASNetMobile","10_EfficientNetB0"]
 
 
 # for feil_name in feil_name:
@@ -16,8 +16,9 @@ feil_name = ["01_cifar100","02_Xception","03_ResNet50","04_ResNet101","05_Incept
 #         file.write("")
 
 for feil_name in feil_name:
-    with open(f"./keras2/keras69_cifar100_{feil_name}_.py","w") as file:
+    with open(f"./torch/torch11_class{feil_name}.py","w") as file:
         file.write("")
+
 
 
 # feil_name = ["01_iris.py","02_cancer.py","03__diabets.py","04_wine.py","05_california.py",\
