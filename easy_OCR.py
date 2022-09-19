560f4bdd94e20ae53d343b005538033ee5a26625
wofvh <wofvh@naver.com>
2022-09-19 14:06:19 +0900

    09/18

--- easy_OCR.py
+++ easy_OCR.py
@@ -1,4 +1,3 @@
-import random
 import pandas as pd
 import numpy as np
 import os
@@ -11,4 +10,14 @@ from torch.utils.data import Dataset, DataLoader
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, Normalizer
 from sklearn.impute import KNNImputer
 from tqdm.auto import tqdm
-import easy_OCR as eo
+import easyocr
+import cv2
+import matplotlib.pyplot as plt
+
+USE_CUDA = torch.cuda.is_available()
+DEVICE  = torch.device('cuda:0' if USE_CUDA else 'cpu')
+print('torch:', torch.__version__,'사용DEVICE :',DEVICE)
+
+
+reader = easyocr.Reader(['en','ko'], gpu=False)  
+
