1038e5ac11b0d06cba1756ab55dcfc5bd01999a4
wofvh <wofvh@naver.com>
2022-09-19 14:31:22 +0900

    09/18

--- easy_OCR.py
+++ easy_OCR.py
@@ -1,3 +1,4 @@
+from tkinter import Widget
 import pandas as pd
 import numpy as np
 import os
@@ -13,6 +14,7 @@ from tqdm.auto import tqdm
 import easyocr
 import cv2
 import matplotlib.pyplot as plt
+import wget
 
 USE_CUDA = torch.cuda.is_available()
 DEVICE  = torch.device('cuda:0' if USE_CUDA else 'cpu')
@@ -20,4 +22,4 @@ print('torch:', torch.__version__,'사용DEVICE :',DEVICE)
 
 
 reader = easyocr.Reader(['en','ko'], gpu=False)  
-
+wget.download https://meeco.kr/files/attach/images/24268070/716/533/032/17a03beb045782788810949204bd20c1.jpg -O 01.jpg
