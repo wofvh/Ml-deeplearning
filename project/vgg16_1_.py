d5809b919020fbc1ebbae14af39a4d32252badfc
wofvh <wofvh@naver.com>
2022-09-30 10:32:13 +0900

    09/30

--- project/vgg16_1_.py
+++ project/vgg16_1_.py
@@ -1,24 +1,30 @@
 from logging import critical
 from torchvision.datasets import CIFAR10
 from torch.utils.data import DataLoader, TensorDataset
-import torch.nn as nn
-import torch.optim as optim
-import numpy as np 
-import torch 
-import torchvision.transforms as tr
+import torch
+import torchvision
 import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+import os
+import torchvision.transforms as tr
+from torchsummary import summary
 USE_CUDA = torch.cuda.is_available()
 DEVICE  = torch.device('cuda:0' if USE_CUDA else 'cpu')
 print('torch:', torch.__version__,'사용DEVICE :',DEVICE)
 
+
 path = './_data/torch_data/'
 
-transf = tr.Compose([tr.Resize(15),tr.ToTensor()])
+transf = tr.Compose([tr.Resize(224,224),tr.ToTensor(),tr.Normalize])
 
-train_dataset = CIFAR10(path, train=True, download=True)  #train=True 학습용 데이터 #download=True 데이터를 다운로드 받겠다
-test_dataset = CIFAR10(path, train=False, download=True)  #train=False 테스트용 데이터 
-# train_dataset = CIFAR10(path, train=True, download=True)
-# test_dataset = CIFAR10(path, train=False, download=True)
+# train_dataset = CIFAR10(path, train=True, download=True)  #train=True 학습용 데이터 #download=True 데이터를 다운로드 받겠다
+# test_dataset = CIFAR10(path, train=False, download=True)  #train=False 테스트용 데이터 
+train_dataset = CIFAR10(path, train=True, download=True)
+test_dataset = CIFAR10(path, train=False, download=True)
 
 x_train , y_train = train_dataset.data/255., train_dataset.targets #데이터를 255로 나누어서 0~1사이의 값으로 만들어준다
 x_test , y_test = test_dataset.data/255., test_dataset.targets 
@@ -28,18 +34,21 @@ x_test = torch.FloatTensor(x_test).to(DEVICE)
 y_train = torch.LongTensor(y_train).to(DEVICE)
 y_test = torch.LongTensor(y_test).to(DEVICE)
 
-print(x_train.shape ,x_test.size()) #torch.Size([50000, 32, 32, 3]) torch.Size([10000, 32, 32, 3])
-print(y_train.shape ,y_test.size()) #torch.Size([50000]) torch.Size([10000])
+print(x_train.shape ,x_test.size())  
+print(y_train.shape ,y_test.size())
+# print(x_test,y_test)
 
-# print(np.min(x_train.numpy())), np.max((x_train.numpy())) #0.0 
+print(np.min(x_train.numpy())), np.max((x_train.numpy())) #0.0 1.0
+
+print(x_train.shape ,x_test.size())  #torch.Size([50000, 32, 32, 3]) torch.Size([10000, 32, 32, 3])
 
 
-# x_train , x_test = x_train.unsqueeze(1),x_test.unsqueeze(1) #차원 바꿔주기 
+x_train , x_test = x_train.reshape(50000,32*32*3), x_test.reshape(10000,32*32*3) #데이터를 3차원으로 만들어준다
 
-x_train, x_test = x_train.reshape(50000,3,32,32),x_test.reshape(10000,3,32,32)
+print(x_train.shape ,x_test.size())  #torch.Size([50000, 3072]) torch.Size([10000, 3072])
 
-print(x_train.shape,x_test.shape) 
-# exit()
+print(y_test.unique())
+
 train_dset = TensorDataset(x_train, y_train)
 test_dset = TensorDataset(x_test, y_test)
 
@@ -47,58 +56,62 @@ test_dset = TensorDataset(x_test, y_test)
 train_loader = DataLoader(train_dset, batch_size=32, shuffle =True)#batch_size=32 한번에 32개씩 불러온다 #shuffle=True 데이터를 섞어준다
 test_loader =  DataLoader(test_dset , batch_size=32, shuffle =False)
 
-print(len(train_loader), len(test_loader)) #1563 313
 
-VGG16 = [64,64,'M',128,128,'M',256,256,256,'M',512,512,512,'M',512,512,512,'M']
 
-def conv_2_block(in_dim,out_dim):
-    model = nn.Sequential(
-        nn.Conv2d(in_dim,out_dim,kernel_size=3,padding=1),
-        nn.ReLU(),
-        nn.Conv2d(out_dim,out_dim,kernel_size=3,padding=1),
-        nn.ReLU(),
-        nn.MaxPool2d(2,2)
-    )
-    return model
-
-def conv_3_block(in_dim,out_dim):
-    model = nn.Sequential(
-        nn.Conv2d(in_dim,out_dim,kernel_size=3,padding=1),
-        nn.ReLU(),
-        nn.Conv2d(out_dim,out_dim,kernel_size=3,padding=1),
-        nn.ReLU(),
-        nn.Conv2d(out_dim,out_dim,kernel_size=3,padding=1),
-        nn.ReLU(),
-        nn.MaxPool2d(2,2)
-    )
-    return model
-class VGG(nn.Module):
-    def __init__(self, base_dim, num_classes=10):
-        super(VGG, self).__init__()
-        self.feature = nn.Sequential(
-            conv_2_block(3,base_dim), #64
-            conv_2_block(base_dim,2*base_dim), #128
-            conv_3_block(2*base_dim,4*base_dim), #256
-            conv_3_block(4*base_dim,8*base_dim), #512
-            conv_3_block(8*base_dim,8*base_dim), #512        
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(3,64,3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(64,64,3, padding=1),nn.LeakyReLU(0.2),
+            nn.MaxPool2d(2,2),
+            
+            nn.Conv2d(64,128,3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(128,128,3, padding=1),nn.LeakyReLU(0.2),
+            nn.MaxPool2d(2,2),
+            
+            nn.Conv2d(128,256,3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(256,256,3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(256,256,3, padding=1),nn.LeakyReLU(0.2),
+            nn.MaxPool2d(2,2),
+            
+            nn.Conv2d(256,512,3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(512,512,3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(512,512,3, padding=1),nn.LeakyReLU(0.2),
+            
+            nn.Conv2d(512, 512, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(512, 512, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(512, 512, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.MaxPool2d(2, 2)
         )
-        self.fc_layer = nn.Sequential(
-            # CIFAR10은 크기가 32x32이므로 
-            nn.Linear(8*base_dim*1*1, 4096),
-            # IMAGENET이면 224x224이므로
-            # nn.Linear(8*base_dim*7*7, 4096),
-            nn.ReLU(True),
-            # nn.Dropout(),
-            nn.Linear(4096, 1000),
-            nn.ReLU(True),
-            nn.Dropout(),
-            nn.Linear(1000, num_classes),
-        )
+        self.avg_pool = nn.AvgPool2d((7, 7))
+        self.Classifier = nn.Linear(512, 1000)
+        
+        self.avg_pool = nn.AvgPool2d(7)
+        #512 1 1
+        self.classifier = nn.Linear(512, 1000)
+        """
+        self.fc1 = nn.Linear(512*2*2,4096)
+        self.fc2 = nn.Linear(4096,4096)
+        self.fc3 = nn.Linear(4096,10)
+        """
 
     def forward(self, x):
-        x = self.feature(x)
-        #print(x.shape)
-        x = x.view(x.size(0), -1)
-        #print(x.shape)
-        x = self.fc_layer(x)
-        return x
+        #print(x.size())
+        features = self.conv(x)
+        #print(features.size())
+        x = self.avg_pool(features)
+        #print(avg_pool.size())
+        x = x.view(features.size(0), -1)
+        #print(flatten.size())
+        x = self.classifier(x)
+        #x = self.softmax(x)
+        return x, features
+        
+        
+        
+        
+model = Net(3).to(DEVICE)
+from torchsummary import summary
+summary(model, (3, 32,32))#torch summary를 사용하면 모델의 구조를 볼수있다
+exit()
