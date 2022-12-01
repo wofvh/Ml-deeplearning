148d097fc02bcbd320a1a8a00f22e1f4255dcfae
이형권 <wofvh@naver.com>
2022-12-01 21:06:47 +0900

    12/01

--- torchtest.py
+++ torchtest.py
@@ -1,1 +1,17 @@
-print("hello")
+#BERT 
+
+import os 
+import re
+import json
+import copy
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-whitegrid')
+
+from tqdm import tqdm
+import tensorflow as tf
+from tensorflow import *
+import keras
+from keras.preprocessing.sequence import pad_sequences
+from keras.callbacks import early_stopping, ModelCheckpoint
