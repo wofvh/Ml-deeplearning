f6ebb2a3c7af2278905b4d52b2e70a721d2463a6
이형권 <wofvh@naver.com>
2022-12-07 20:19:13 +0900

    12/07

--- ml/m56_BayesianOptimization_xgb_12_kaggle_house.py
+++ ml/m56_BayesianOptimization_xgb_12_kaggle_house.py
@@ -17,7 +17,6 @@ x_train, x_test, y_train, y_test = train_test_split(
     x, y, train_size=0.8, random_state=123  
 )
 
-
 Scaler = StandardScaler() #Bagging 할때 스케일러 필수 
 x_train = Scaler.fit_transform(x_train)
 x_test = Scaler. transform(x_test)
