b668027669fc3e18cbc735e68b1f206b85add830
이형권 <wofvh@naver.com>
2022-12-13 19:36:05 +0900

    12/13

--- ml/m56_BayesianOptimization_xgb_11_kaggle_bike.py
+++ ml/m56_BayesianOptimization_xgb_11_kaggle_bike.py
@@ -12,7 +12,6 @@ datasets = load_breast_cancer()
 x, y = datasets.data, datasets.target
 print(x.shape, y.shape) # (506, 13) (506,)
 
-
 x_train, x_test, y_train, y_test = train_test_split(
     x, y, train_size=0.8, random_state=123  
 )
